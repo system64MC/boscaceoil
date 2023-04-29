d8ab2fe440115a58f0edd86492c46d70e438e35e
Gustav <sir.gustav.the.coder@gmail.com>
2023-04-29 10:00:24 +0200

    Port listclass

--- src/list.cc
+++ src/list.cc
@@ -1,48 +1,51 @@
+#include <vector>
+#include <string>
+
 namespace bosca {
-	
-	
-	
-		
-	struct listclass	{
-		void listclass() {
+	struct listclass {
+		listclass() {
 			for (int i = 0; i < 30; i++) {
-				item.push("");
+				item.push_back("");
 			}
 			clear();
 		}
-		
+
 		void clear() {
 			numitems = 0;
 			active = false;
 			x = 0; y = 0;
 			selection = -1;
 		}
-		
+
 		void init(int xp, int yp) {
 			x = xp; y = yp; active = true;
 			getwidth();
+#ifdef GFX
 			h = numitems * gfx.linesize;
+#endif
 		}
-		
+
 		void close() {
 			active = false;
 		}
-		
+
 		void getwidth() {
 			w = 0;
 			int temp;
 			for (int i = 0; i < numitems; i++) {
+#ifdef GFX
 				temp = gfx.len(item[i]);
+#endif
 				if (w < temp) w = temp;
 			}
 			w += 10;
 		}
-		
+
 		std::vector<std::string> item;
 		int numitems;
 		bool active;
-		int x, int y, int w, int h;
+		int x; int y; int w; int h;
 		int type;
 		int selection;
-	}
+	};
 }
