a76e5545a400c50147c8d093e8604d4df4c3babb
Gustav <sir.gustav.the.coder@gmail.com>
2023-04-29 16:54:43 +0200

    Add dummy implementation

---
+++ src/rect.cc
@@ -0,0 +1,17 @@
+#include "rect.h"
+
+#include <cassert>
+
+
+namespace bosca
+{
+	Rectangle::Rectangle(int, int, int, int)
+	{
+		assert(false && "not implemented");
+	}
+
+	void Rectangle::setTo(int, int, int, int)
+	{
+		assert(false && "not implemented");
+	}
+}
---
+++ src/si-voice.cc
@@ -0,0 +1,12 @@
+#include "si-voice.h"
+
+#include <cassert>
+
+namespace sion
+{
+	void Voice::setFilterEnvelop(int, int, int)
+	{
+		assert(false && "not implemented");
+	}
+}
+
