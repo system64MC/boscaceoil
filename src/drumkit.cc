e9072b94901c7989c430bde874dbfc796da1b8ce
Gustav <sir.gustav.the.coder@gmail.com>
2023-04-29 08:50:47 +0200

    Port drumkit

--- src/drumkit.cc
+++ src/drumkit.cc
@@ -1,37 +1,52 @@
+#include <string>
+#include <vector>
+
+namespace sion
+{
+	struct Voice
+	{
+		struct ChannelParam
+		{
+			int cutoff;
+			int resonance;
+		};
+
+		ChannelParam channelParam;
+		int velocity;
+		bool updateVolumes;
+
+		void setFilterEnvelop(int, int cutoff, int resonance);
+	};
+}
+
 namespace bosca {
-	
-	
-	
-	
-	import org.si.sion.SiONVoice;
-	
 	struct Drumkit {
-		void Drumkit() {
+		Drumkit() {
 			size = 0;
 		}
-		
+
 		void updatefilter(int cutoff, int resonance) {
 			for (int i = 0; i < size; i++) {
-				if(voicelist[i].channelParam.cutoff != cutoff || voicelist[i].channelParam.resonance != resonance){
+				if (voicelist[i].channelParam.cutoff != cutoff || voicelist[i].channelParam.resonance != resonance) {
 					voicelist[i].setFilterEnvelop(0, cutoff, resonance);
 				}
 			}
 		}
-		
+
 		void updatevolume(int volume) {
 			for (int i = 0; i < size; i++) {
-				if(voicelist[i].velocity!=volume){
+				if (voicelist[i].velocity != volume) {
 					voicelist[i].updateVolumes = true;
 					voicelist[i].velocity = volume;
 				}
 			}
 		}
-		
-		std::vector<SiONVoice> voicelist = new std::vector<SiONVoice>;
+
+		std::vector<sion::Voice> voicelist;
 		std::vector<std::string> voicename;
 		std::vector<int> voicenote;
 		std::vector<int> midivoice;
 		std::string kitname;
 		int size;
-	}
+	};
 }
